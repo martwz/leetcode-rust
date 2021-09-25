1e2613e105107324767b0d60fa12321b6e0749f1
Martin Weiss <martinweiss@posteo.de>
2021-09-25 09:02:41 -0700

    Add comment

--- src/leetcode/challenge/shortest_path.rs
+++ src/leetcode/challenge/shortest_path.rs
@@ -1,6 +1,7 @@
 use std::collections::{HashSet};
 use std::iter::FromIterator;
 
+// https://leetcode.com/problems/shortest-path-in-a-grid-with-obstacles-elimination/
 impl Solution {
     pub fn shortest_path(grid: Vec<Vec<i32>>, k: i32) -> i32 {
         fn bfs(grid: &Vec<Vec<i32>>, pos: (i32, i32), obstacles_left: i32, mut visited: HashSet<(i32, i32)>, paths: &mut Vec<Vec<(i32, i32)>>) {
